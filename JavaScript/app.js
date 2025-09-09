c00a73a926992c2621c5a679950061f25302b957
VazquezMartinezEdgarUriel <124049760@upq.edu.mx>
2025-09-08 21:59:43 -0600

    Summari javascript

---
+++ JavaScript/app.js
@@ -0,0 +1,36 @@
+console.log({"username": "ryan",
+    "score": 70.4});
+
+
+
+
+
+
+
+
+
+
+/*
+
+Tipos de datos:
+"Hello World"  string
+
+100000  number
+
+booleam
+true
+false
+
+array
+["Joe","ryan","Martha"]
+[1,2,3]
+[true,flase,true,false]
+
+ object
+{
+    "Username":"ryan",
+    "score": 70.4,
+    "hours": 14,
+    "proffesional": true
+}
+    */
