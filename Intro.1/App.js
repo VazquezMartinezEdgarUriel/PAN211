19c944b961363090a51a0206b64297033282ffa1
VazquezMartinezEdgarUriel <124049760@upq.edu.mx>
2025-10-08 08:47:01 -0600

    Componentes Props y State
    Desarrollo de interacción de  funciones explicado y los componentes que contamos hay mismo.

--- Intro.1/App.js
+++ Intro.1/App.js
@@ -7,23 +7,19 @@ import { Button } from 'react-native-web';
 //2. Main: Zona de componentes
 export default function App() {
 
-  const[contador, setContador] = useState(0);
-  
-
+  const[contador,setContador]=useState(0);
   return (
-
-
     <View style={styles.container}>
       <Text> Contador: {contador} </Text>
       <Button title='Agregar' onPress={()=>setContador(contador+1)} />
-      <Button title='Quitar' onPress={()=>setContador(contador-1)} />
       <Button title='Reiniciar' onPress={()=>setContador(0)} />
+      <Button title='Quitar' onPress={()=>setContador(contador-1)} />
       <StatusBar style="auto" />
-    </View> // se tiliza para visualizar el texto
+    </View> 
   );
 }
 
-//3. Estilos: Zona estilos
+//3 Styles: Zona de Estilos para el Componente
 const styles = StyleSheet.create({
   container: {
     flex: 1,
