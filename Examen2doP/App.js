0bfc8d1d13e7ed4cc3d2037721ed50f1b61ad72b
VazquezMartinezEdgarUriel <124049760@upq.edu.mx>
2025-11-06 19:51:29 -0600

    1er Commit

--- Examen2doP/App.js
+++ Examen2doP/App.js
@@ -1,22 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState, useEffect} from 'react';
+import { StyleSheet, Text, View, ImageBackground,Dimensions } from 'react-native';
+
+const BackgroundImage = require( '../assets/5011.jpg' );
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Bienbenido.........</Text>
-      
-      <Text>Proximamente........</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setShowSplash(false);
+    }, 3000); 
+    return () => clearTimeout(timer);
+  }, []);
+  if (setShowSplash){
+    return(
+      <View style={styles.container}>
+    <ImageBackground source={BackgroundImage}
+      style={styles.background}
+      resizeMode='cover'> 
+      <View style={styles.overlay}>
+        <Text style={styles.tittle}>Los Vazquez</Text>
+        <Text style={styles.tittle}>Un restaurante comprometido</Text>
+        </View>
+    </ImageBackground>
+    </View>
+    )}
+  }
+  const windowWidth = Dimensions.get('window').width;
+  const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  tittle:{
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: 'black',
+    textAlign: 'center',
+  },
+  mainScreen: {
+    flex:1,
+    justifyContent: 'center',
+    alignItems:'center',
+  },
+  mineText:{
+    fontSize: 20,
+    color: 'black',
+    textAlign: 'center',
+  },
+  background:{
+    flex:1,
+    width: windowWidth,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  overlay:{
+    backgroundColor: 'white',
+    padding: 20,
+    borderRadius: 10,
+  },  
 });
