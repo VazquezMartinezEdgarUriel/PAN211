d3bd90aa8a2cdddf3c4725e4a64cd391fd578a0b
VazquezMartinezEdgarUriel <124049760@upq.edu.mx>
2025-11-06 20:32:45 -0600

    CommitLimte
    Entrega 8:30

--- Examen2doP/App.js
+++ Examen2doP/App.js
@@ -1,8 +1,12 @@
-import { useState, useEffect} from 'react';
-import { StyleSheet, Text, View, ImageBackground,Dimensions } from 'react-native';
+import { useState, useEffect } from 'react';
+import React, { Component }from 'react';
+import { StyleSheet, Text, View, ImageBackground,Dimensions, ScrollView } from 'react-native';
 
-const BackgroundImage = require( '../assets/5011.jpg' );
-
+const BackgroundImage = require('.../assets/5011.jpg')
+const BackgroundImage2 = require('.../assets/4427.jpg')
+const BackgroundImage3 = require('.../assets/Agua1212.jpg')
+const BackgroundImage4 = require('.../assets/Pizza11.jpg')
+const BackgroundImage5 = require('.../assets/5055.jpq')
 export default function App() {
 
   useEffect(() => {
@@ -22,6 +26,53 @@ export default function App() {
         <Text style={styles.tittle}>Un restaurante comprometido</Text>
         </View>
     </ImageBackground>
+    <ScrollView style={styles.scrollArea} 
+        contentContainerStyle={styles.scrollContent} 
+        showVerticalScrollIndicator={true}> 
+        <Text style={style.items}>Comida </Text>
+        <ImageBackground source={BackgroundImage2}
+        styles={ background}
+        resizeMode='cover'>
+        <View style={styles.mainScreen}>
+        <Text style={styles.mainText}>Hamburguesa</Text>
+        <Text style={styles.mainText}>Rica Hamburguesa a termino medio, carne 70/30 </Text>
+        <Text style={styles.mainText}>$125</Text>
+        </View>
+        </ImageBackground>
+        <ImageBackground source={BackgroundImage4}
+        styles={ background}
+        resizeMode='cover'>
+        <View style={styles.mainScreen}>
+        <Text style={styles.mainText}>Pizza</Text>
+        <Text style={styles.mainText}>Rica Pizza recien echa, se utiliza maza madre de primera calidad  </Text>
+        <Text style={styles.mainText}>$250 Pizza Familiar</Text>
+        </View>
+        </ImageBackground>
+    </ScrollView>  
+    <ScrollView style={styles.scrollArea} 
+        contentContainerStyle={styles.scrollContent} 
+        showVerticalScrollIndicator={true}> 
+        <Text style={style.items}>Bebida</Text>
+         <ImageBackground source={BackgroundImage5}
+        styles={ background}
+        resizeMode='cover'>
+        <View style={styles.mainScreen}>
+        <Text style={styles.mainText}>Coca Cola</Text>
+        <Text style={styles.mainText}>70/30 </Text>
+        <Text style={styles.mainText}>$45</Text>
+        </View>
+        </ImageBackground>
+        <ImageBackground source={BackgroundImage3}
+        styles={ background}
+        resizeMode='cover'>
+        <View style={styles.mainScreen}>
+        <Text style={styles.mainText}>Pizza</Text>
+        <Text style={styles.mainText}>Rica Pizza recien echa, se utiliza maza madre de primera calidad  </Text>
+        <Text style={styles.mainText}>$250 Pizza Familiar</Text>
+        </View>
+        </ImageBackground>
+
+    </ScrollView>    
     </View>
     )}
   }
